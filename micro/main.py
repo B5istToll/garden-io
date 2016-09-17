e319dba8ad20133f15bd7b508c640be61f5ee84a
Sandro Meier <sandro.meier@fidelisfactory.ch>
2016-09-17 10:42:53 +0200

    Add plants endpoint

--- micro/main.py
+++ micro/main.py
@@ -1,9 +1,24 @@
 from flask import Flask
+from flask import json
+from flask import jsonify
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+
+@app.route('/api/plants')
+def get_plants():
+    # Load the plant data
+    with open('data/plants.json') as f:
+        plant_data = json.load(f)
+
+    return jsonify(plant_data)
+
+
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=6677)
