87080d9a96de136ff5d69a133d2d73fa951ed704
Sandro Meier <sandro.meier@fidelisfactory.ch>
2016-09-17 13:27:22 +0200

    Fix problem

--- micro/garden_logic.py
+++ micro/garden_logic.py
@@ -1,3 +1,5 @@
+from datetime import date, datetime
+
 from flask import json
 import random
 
@@ -46,12 +48,86 @@ def create_garden(width, height, path):
         json.dump(data, f)
 
 
-def update_garden(path):
+def update_garden(path, x, y):
     """
     Update the given garden with new proposals.
+    X and Y show the location that should be updated.
     :param path: The path where the garden is stored.
     """
     pass
+
+class Utility:
+
+    date_format = '%d.%m.%Y'
+
+    @staticmethod
+    def dates_collide(start1, end1, start2, end2):
+
+        start1 = datetime.strptime(Utility.date_format, start1)
+        end1 = datetime.strptime(Utility.date_format, end1)
+        start2 = datetime.strptime(Utility.date_format, start2)
+        end2 = datetime.strptime(Utility.date_format, end2)
+
+        # Let's order them first
+        if start2 > start1:
+            tmp_start = start1
+            tmp_end = end1
+            start1 = start2
+            end1 = end2
+            start2 = tmp_start
+            end2 = tmp_end
+
+        return end1 > start2
+
+
+class Garden:
+    """
+    A class to represent a garden.
+    """
+
+    def __init__(self, data_path):
+        self.data_path = data_path
+        with open(data_path) as f:
+            self.data = json.load(f)
+
+    def save(self):
+        with open(self.data_path, 'w') as f:
+            json.dump(self.data, f)
+
+    def plant(self, x, y, plant_name, date):
+        """
+        Plant a plant at a given location and date.
+        """
+        plants = Plants()
+        plant_info = plants.get_complete_info(plant_name)
+        plant_crop_date = plants.get_crop_date(plant_info, date)
+
+        # Remove all plants during that time frame.
+        relevant_tiles = self.get_tiles(x, y)
+        # Check if these tiles collide with the new planted plant.
+        for tile in relevant_tiles:
+            if Utility.dates_collide(date, plant_crop_date, tile['plant_date'], tile['crop_date']):
+                self.data['tiles'].remove(tile)
+
+        # And finally insert the new plant.
+        self.data['tiles'].append({
+                'location': {
+                    'x': x,
+                    'y': y
+                },
+                'plant': plant_info,
+                'plant_date': date,
+                'crop_date': plant_crop_date,
+                'duration': plant_info['duration'],
+                'proposal': False
+            })
+
+    def get_tiles(self, x, y):
+        """
+        Get all tiles with a specific location.
+        """
+        valid_tiles = filter(lambda tile: tile['location']['x'] == x and tile['location']['y'] == y, self.data['tiles'])
+        return list(valid_tiles)
 
 
 class Plants:
@@ -77,6 +153,9 @@ class Plants:
 
         return random.choice(list(valid_plants))
 
+    def get_complete_info(self, plant_name):
+        valid_plants = filter(lambda x: x['name'] == plant_name, self.plants)
+        return list(valid_plants)[0]
 
     def get_plant_and_crop_date(self, plant):
         """
@@ -88,11 +167,18 @@ class Plants:
         plant_month = random.choice(plant['plant'])
         plant_date = '%.2d.%.2d.2016' % (plant_day, plant_month)
 
-        # Calculate crop date
-        crop_day_total = plant_day + plant['duration'] * 7
-        crop_day = crop_day_total % 28
-        crop_months = plant_month + (crop_day_total - crop_day) / 28
-        crop_date = '%.2d.%.2d.2016' % (crop_day, crop_months)
+        crop_date = self.get_crop_date(plant, plant_date)
 
-        return (plant_date, crop_date)
+        return plant_date, crop_date
+
+    def get_crop_date(self, plant, plant_date):
+
+        day, month, year = plant_date.split('.')
+
+        # Calculate crop date
+        crop_day_total = int(day) + plant['duration'] * 7
+        crop_day = crop_day_total % 28
+        crop_months = int(month) + (crop_day_total - crop_day) / 28
+        crop_date = '%.2d.%.2d.2016' % (crop_day, crop_months)
+        return crop_date
 
