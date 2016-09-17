c56b9a93cc8258e75f66e372c06181cb3a00b925
Sandro Meier <sandro.meier@fidelisfactory.ch>
2016-09-17 12:14:24 +0200

    Add headers

--- micro/main.py
+++ micro/main.py
@@ -37,6 +37,13 @@ def get_garden():
     return jsonify(garden_data)
 
 
+@app.after_request
+def after_request(response):
+    # Add Access control headers
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=6677, debug=True)
