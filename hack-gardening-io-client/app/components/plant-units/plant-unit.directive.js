5b10b46c0539843c2701b76c86fd13c9a1a5cee3
Julian Förster <fjulian@student.ethz.ch>
2016-09-18 02:17:27 +0200

    Working on modal

--- hack-gardening-io-client/app/components/plant-units/plant-unit.directive.js
+++ hack-gardening-io-client/app/components/plant-units/plant-unit.directive.js
@@ -13,7 +13,7 @@ angular.module('gardenDesigner').directive('plantUnit', function () {
         templateUrl: 'components/plant-units/plant-unit.template.html',
         controller: 'plantUnitController'
     };
-}).controller('plantUnitController', function ($scope, $rootScope) {
+}).controller('plantUnitController', function ($scope, $rootScope, $uibModal, backendService) {
     $scope.plantName = $scope.plantObj.plant.name;
     $scope.plantState = $scope.plantObj.state;
 
@@ -28,28 +28,31 @@ angular.module('gardenDesigner').directive('plantUnit', function () {
         $scope.yield = false;
         $scope.progressBar = false;
         $scope.progress = 0;
-        if ($scope.plantState == 'suggestion') {
+        if ($scope.plantState === 'suggestion') {
             $scope.change = true;
             $scope.confirm = true;
-        } else if ($scope.plantState == 'scheduled') {
-            console.log("Dates. Current: ", $scope.currentDate, "  Seed: ", $scope.seedDate);
+        } else if ($scope.plantState === 'scheduled') {
+            //console.log("Dates. Current: ", $scope.currentDate, "  Seed: ", $scope.seedDate);
             if ($scope.currentDate < $scope.seedDate) {
                 $scope.abort = true;
             } else {
                 $scope.abort = true;
                 $scope.seed = true;
             }
-        } else if ($scope.plantState == 'in_progress') {
-            console.log("Dates. Current: ", $scope.currentDate, "  Harvest: ", $scope.harvestDate);
+        } else if ($scope.plantState === 'in_progress') {
+            //console.log("Dates. Current: ", $scope.currentDate, "  Harvest: ", $scope.harvestDate);
             $scope.abort = true;
             $scope.progressBar = true;
             var daysElapsed = ($scope.currentDate - $scope.seedDate)/1000/60/60/24;
             var daysToGrow = ($scope.harvestDate - $scope.seedDate)/1000/60/60/24;
             $scope.progress = daysElapsed/daysToGrow*100;
-            console.log("Fortschritt: ",$scope.progress);
-            console.log("Schon vergangen: ",daysElapsed);
-            console.log("Tage insgesamt: ",daysToGrow);
-        } else if ($scope.plantState = 'ready_to_harvest') {
+            if ($scope.progress >=100) {
+                $scope.plantState='ready_to_harvest';
+            }
+            //console.log("Fortschritt: ",$scope.progress);
+            //console.log("Schon vergangen: ",daysElapsed);
+            //console.log("Tage insgesamt: ",daysToGrow);
+        } else if ($scope.plantState === 'ready_to_harvest') {
             $scope.yield = true;
         }
     };
@@ -57,6 +60,9 @@ angular.module('gardenDesigner').directive('plantUnit', function () {
 
     $rootScope.$on('dateChanged', function (event, dt) {
         $scope.currentDate = dt;
+        if ($scope.currentDate >= $scope.harvestDate && $scope.plantState === 'in_progress') {
+            $scope.plantState = 'ready_to_harvest';
+        }
         //console.log("Dates. Current: ", $scope.currentDate, "  Seed: ", $scope.seedDate);
         $scope.setFlags();
     });
@@ -72,6 +78,94 @@ angular.module('gardenDesigner').directive('plantUnit', function () {
         $scope.plantState = 'in_progress';
         $scope.setFlags();
         // TODO inform backend
-    }
+    };
+
+    $scope.harvestClicked = function () {
+        $scope.plantState = 'suggestion';
+        $scope.setFlags();
+        // TODO inform backend
+        // TODO get new suggestion from backend
+    };
+    
+    $scope.changeClicked = function () {
+        $ctrl.open();
+    };
+
+
+    // Modal ----------------------------------------------------
+
+    var $ctrl = this;
+
+    $scope.plants = {};
+    var promise = backendService.getPlants();
+    promise.then(function (data) {
+        $scope.plants = data;
+        //console.log("dara: ", data);
+        $ctrl.items = $scope.plants.data.plants;
+    });
+
+
+
+    $ctrl.animationsEnabled = true;
+
+    $ctrl.open = function (size) {
+        var modalInstance = $uibModal.open({
+            animation: $ctrl.animationsEnabled,
+            ariaLabelledBy: 'modal-title',
+            ariaDescribedBy: 'modal-body',
+            templateUrl: 'myModalContent.html',
+            controller: 'plantModalCtrl',
+            controllerAs: '$ctrl',
+            size: size,
+            resolve: {
+                items: function () {
+                    return $ctrl.items;
+                }
+            }
+        });
+
+        modalInstance.result.then(function (selectedItem) {
+            $ctrl.selected = selectedItem;
+        }, function () {
+            console.log('Modal dismissed at: ' + new Date());
+        });
+    };
+
+});
+
+angular.module('gardenDesigner').controller('plantModalCtrl', function ($uibModalInstance, items) {
+    var $ctrl = this;
+    $ctrl.items = items;
+    $ctrl.selected = {
+        item: $ctrl.items[0]
+    };
+
+    $ctrl.ok = function () {
+        $uibModalInstance.close($ctrl.selected.item);
+    };
+
+    $ctrl.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+
+    $scope.getImgPath = function (name) {
+        if (name == 'Brokkoli') return "components/img/brokkoli.png";
+        if (name == 'Rotkohl') return "components/img/rotkohl.png";
+        if (name == 'Kartoffeln') return "components/img/kartoffel.png";
+        if (name == 'Moehren') return "components/img/moehre.png";
+        if (name == 'Radieschen') return "components/img/radieschen.png";
+        if (name == 'Rote Beete') return "components/img/rotebeete.png";
+        if (name == 'Feldsalat') return "components/img/feldsalat.png";
+        if (name == 'Auberginen') return "components/img/auberginen.png";
+        if (name == 'Kuerbis') return "components/img/kuerbis.png";
+        if (name == 'Zucchini') return "components/img/zucchini.png";
+        if (name == 'Peperoni') return "components/img/peperoni.png";
+        if (name == 'Tomaten') return "components/img/tomaten.png";
+        if (name == 'Zuckermais') return "components/img/zuckermais.png";
+        if (name == 'Lauch') return "components/img/lauch.png";
+        if (name == 'Zwiebeln') return "components/img/zwiebeln.png";
+        if (name == 'Knoblauch') return "components/img/knoblauch.png";
+        if (name == 'Weisser Spargel') return "components/img/spargel.png";
+    };
 
 });
